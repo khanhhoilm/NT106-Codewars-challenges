ac2d8fb237d98a6d6a1287e6a63d573688db5f7c
khanhhoilm <khanhhoilm@gmail.com>
2022-04-21 09:40:52 +0700

    inital project

---
+++ Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_challenges_fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string bmi = codewars_fundamentals.Bmi(50, 150);
+            Console.WriteLine("YOUR BMI: " + bmi);
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
