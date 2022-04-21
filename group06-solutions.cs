5352d1f311c3b25ef4811b7108e7fe1a78e922d9
Long TIU 20 <84081009+uit-20520625@users.noreply.github.com>
2022-04-21 14:22:56 +0700

    Update group06-solutions.cs

--- group06-solutions.cs
+++ group06-solutions.cs
@@ -1,1 +1,11 @@
+//Convert a Number to a String!
+//Tran Hoang Long 20520625
+using System;
 
+public class Kata
+{
+  public static string NumberToString(int num)
+  {
+    return num.ToString();
+  }
+}
