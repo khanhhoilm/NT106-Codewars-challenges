a650bd71c76b3ad62a1c50d4c959341aef778aea
nhungthai__jj <20521726@gm.uit.edu.vn>
2022-04-21 20:35:15 +0700

    Group12

---
+++ Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/group12-solutions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_challenges_fundamentals
+{
+    internal class group12_solutions
+    {
+        //Đề bài1: Write a function that takes an array of numbers and returns the sum of the numbers.
+        //The numbers can be negative or non-integer.
+        //If the array does not contain any numbers then you should return 0.
+        public static double SumArray(double[] array)
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            return sum;
+        }
+        //Đề bài 2: Very simple, given an integer or a floating-point number, find its opposite.
+        public static int Opposite(int number)
+        {
+            int oppNum = 0 - number;
+            return oppNum;
+        }
+    }
+}
+// Thành viên nhóm 12:
+// Thái Thị Nhung: 20521726
+// Lê Thị Anh Thư: 20521986
+// Nguyễn Thị Kim Thùy: 20522004
+// Nguyễn Thị Trà Tiên: 20520804
+// Đỗ Thị Minh Oanh: 19521982
+
