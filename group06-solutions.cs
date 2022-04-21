2a22ca95cf64c188814f54f0a6d276a035657ad5
Long TIU 20 <84081009+uit-20520625@users.noreply.github.com>
2022-04-21 14:40:52 +0700

    Update group06-solutions.cs

--- group06-solutions.cs
+++ group06-solutions.cs
@@ -9,3 +9,18 @@ public class Kata
     return num.ToString();
   }
 }
+
+//Are You Playing Banjo?
+using System;
+
+public class Kata
+{
+  public static string AreYouPlayingBanjo(string name)
+  {
+    if (name[0] == 'R' || name[0] == 'r') {
+      return (name + " plays banjo");
+      }
+      return (name + " does not play banjo");
+    
+  }
+}
