3ea652eabc4cc6c197b57d03009fb2339ca4f271
FlyingPeg <101076175+FlyingPeg@users.noreply.github.com>
2022-04-21 14:54:43 +0700

    Create group13-Solutions.cs

---
+++ group13-Solutions.cs
@@ -0,0 +1,13 @@
+using System;
+//Đỗ Xuân Long - 20520619
+
+//Bài 1:
+//Write a function which converts the input string to uppercase.
+public class Kata
+  {
+        public static string MakeUpperCase(string str)
+        {
+             return str.ToUpper();
+        }
+
+  }
