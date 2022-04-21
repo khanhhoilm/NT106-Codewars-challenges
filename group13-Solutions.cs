b0c1235c5866526c03a2ca772dacb6fba8bdab89
FlyingPeg <101076175+FlyingPeg@users.noreply.github.com>
2022-04-21 15:21:58 +0700

    Update group13-Solutions.cs

--- group13-Solutions.cs
+++ group13-Solutions.cs
@@ -10,3 +10,40 @@ public class Kata
              return str.ToUpper();
         }
 }
+//Bài 2:
+//Convert number to reversed array of digits
+public static long[] Digitize(long n)
+    {
+      // Code goes here
+      string str = n.ToString();
+      char[] chArr= str.ToCharArray();
+      Array.Reverse(chArr);
+      long[] longArr = new long[chArr.Length];
+      for (int i = 0; i < longArr.Length; i++)
+      {
+        long number;
+        if (Int64.TryParse(chArr[i].ToString(), out number))
+        {
+          longArr[i] = number;
+        }
+      }
+      return longArr;
+    }
+//Bài 3:
+//The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+public static int СenturyFromYear(int year)
+  {
+            int t=0;
+            string s = year.ToString();
+            if (s[2] == '0' && s[3] == '0')
+                {
+                    t = year/100;
+                    
+                }
+            else
+            {
+                t = year/100;
+                t += 1;
+            }
+            return t;
+  }
