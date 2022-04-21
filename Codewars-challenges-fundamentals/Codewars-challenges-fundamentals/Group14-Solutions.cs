7437dc7bcb9909c63d1139f9a0193eef6e1f77f8
Hong Truong Vinh <hongvinhkrn@gmail.com>
2022-04-21 14:21:33 +0700

    Create branch Group 14

---
+++ Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/Group14-Solutions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_challenges_fundamentals
+{
+    class Group14_Solutions
+    {
+        //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+        public string boolToWord(bool word)
+        {
+            if (word)
+            {
+                return "Yes";
+            }
+            return "No";
+        }
+
+
+    }
+
+}
