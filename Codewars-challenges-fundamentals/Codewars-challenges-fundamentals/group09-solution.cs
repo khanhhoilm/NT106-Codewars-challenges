04090db402fb905a229507f0da1cf7f8e62eaec4
Nguyễn Anh Tài <vipkuteaz1122@gmail.com>
2022-04-21 14:24:20 +0700

    Add File

--- Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/group09-solution.cs
+++ Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/group09-solution.cs
@@ -8,5 +8,22 @@ namespace Codewars_challenges_fundamentals
 {
     class group09_solution
     {
+            public static string AlphabetPosition(string text)
+            {
+                text.Split(' ').join('');
+                var chari = "";
+                var arr = [];
+                var alphabet = "abcdefghijklmnopqrstuvwxyz".Split('');
+                for (var i = 0; i < text.len; i++)
+                {
+                    chari = text.charAt(i).toLowerCase();
+                    if (alphabet.indexOf(chari) > -1)
+                    {
+                        arr.push(alphabet.indexOf(chari));
+                    }
+                }
+                return arr;
+            }
+        
     }
 }
