678110ea566123ddf2548ad29cf964fdba1ff6e0
Hong Truong Vinh <hongvinhkrn@gmail.com>
2022-04-21 15:09:25 +0700

    Nhom 14

--- Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/Group14-Solutions.cs
+++ Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/Group14-Solutions.cs
@@ -70,6 +70,19 @@ namespace Codewars_challenges_fundamentals
             return min;
         }
 
+
+        //Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+        //Your task is to calculate how many blank pages do you need.If n< 0 or m< 0 return 0.
+        public static int Paperwork(int n, int m)
+        {
+            if (m * n < 0 || (m < 0 && n < 0))
+            {
+
+                return 0;
+            }
+            return m * n;
+        }
+
     }
 
 }
