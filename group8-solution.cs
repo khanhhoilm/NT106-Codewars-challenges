c84cfeaf8770540a0a6ac0952d10bdf265a7a0d4
Huynh Nhut Linh <20521530@gm.uit.edu.vn>
2022-04-21 15:08:40 +0700

    no message

---
+++ group8-solution.cs
@@ -0,0 +1,32 @@
+/*
+            Huynh Nhut Linh 20521530
+         */
+
+
+/*Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.*/
+
+public static string boolean_to_string(bool b)
+{
+    return b.ToString();
+}
+
+/*Very simple, given an integer or a floating-point number, find its opposite.
+*/
+public static int Opposite(int number)
+{
+    return -number;
+}
+
+/*Given an array of integers, return a new array with each value doubled.*/
+
+public static int[] Maps(int[] x)
+{
+    for (int i = 0; i < x.Lenght; i++)
+    {
+
+    }
+
+    return x;
+}
