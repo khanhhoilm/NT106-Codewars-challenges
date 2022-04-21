71c11cbf362c3a0c27c21c5530487d995c97370c
Hong Truong Vinh <hongvinhkrn@gmail.com>
2022-04-21 14:58:32 +0700

    Nhom14

--- Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/Group14-Solutions.cs
+++ Codewars-challenges-fundamentals/Codewars-challenges-fundamentals/Group14-Solutions.cs
@@ -18,6 +18,57 @@ namespace Codewars_challenges_fundamentals
             return "No";
         }
 
+        //Code as fast as you can! You need to double the integer and return it.
+        public static int DoubleInteger(int n)
+        {
+            return n * 2;
+        }
+
+        //Write a function to split a string and convert it into an array of words.
+        public static string[] StringToArray(string str)
+        {
+            string[] arr = str.Split(' ');
+            return arr;
+        }
+
+        //Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+        public static int TotalPoints(string[] games)
+        {
+
+            int point = 0;
+
+            foreach (string item in games)
+            {
+                string[] str = item.Split(':');
+                int x = int.Parse(str[0]);
+                int y = int.Parse(str[1]);
+                if (x > y)
+                {
+                    point += 3;
+                }
+                else if (x == y)
+                {
+                    point += 1;
+                }
+            }
+
+            return point;
+        }
+
+        //Given an array of integers your solution should find the smallest integer
+        public static int FindSmallestInt(int[] args)
+        {
+            int min = args[0];
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] < min)
+                {
+                    min = args[i];
+                }
+            }
+
+            return min;
+        }
 
     }
 
